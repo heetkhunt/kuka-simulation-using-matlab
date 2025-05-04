881908f986b40da8a2b148a79bda64ae4d88733d
heetkhunt <hitkhunt@gmail.com>
2025-05-04 18:33:11 +0200

    Update RTAssignment.m

--- RTAssignment.m
+++ RTAssignment.m
@@ -1,47 +1,47 @@
-% task a)
+
 myRobot = loadrobot('kukaIiwa7');
 showdetails(myRobot)
 
-% task b)
+
 gripper=importrobot('THWSGripper.URDF');
 
-% task c)
+
 addSubtree(myRobot,'iiwa_link_ee_kuka',gripper);
 
-% task d)
+
 myFigure=interactiveRigidBodyTree(myRobot);
 
-% task e)
+
 worktop = collisionBox(1.2, 1.2, 0.05);
 worktop.Pose = trvec2tform([0.5 ,0 ,-0.025]);
 [~,worktop1]=show(worktop);
 worktop1.FaceColor = [0.7 0.7 0.7];
 
-% task f)
+
 cuboid = collisionBox(0.08, 0.08, 0.15);
 cuboid.Pose = trvec2tform([0.6, -0.3, 0.075]);
 [~,cuboidRed] = show(cuboid);
 cuboidRed.FaceColor = [1 0 0];
 
-% task g)
+
 addConfiguration(myFigure);
 
-% task h)
+
 myFigure.Configuration=[-0.463339512287680;0.784656954439209;-0.021837121263641;-1.047881830372538;0.028298887046785;1.305358514624127;-0.502222620571122;0;0];
 addConfiguration(myFigure);
 
-% task i)
+
 myFigure.Configuration=[-0.451180851379378;0.940279066858544;-0.021396674441368;-1.153267977113472;0.017643667285712;1.048571254605699;-0.470912403776183;0;0];
 addConfiguration(myFigure);
 
-% task j)
+
 myFigure.Configuration=[-0.539253400661772;0.816895479614483;0.114775898358588;-1.121329651312853;-0.075712516629101;1.197367239628034;-0.448501699980762;0;0];
 addConfiguration(myFigure);
 
-% task k)
+
 myPose = myFigure.MarkerBodyPose;
 
-% task l)
+
 myPose(1,4) = 0.6;
 myPose(2,4) = 0.3;
 myPose(3,4) = 0.3;
@@ -50,22 +50,22 @@ weights = [1 1 1 1 1 1];
 initguess = myRobot.homeConfiguration; 
 [config,info] = myIK('TCP',myPose,weights,initguess);      
 
-% task m)
+
 for i = 1:9 
     myFigure.Configuration(i) = config(i).JointPosition; 
 end 
 addConfiguration(myFigure);
 
-% task n)
+
 myFigure.Configuration = zeros(9,1);
 addConfiguration(myFigure);
 
-% task o)
+
 NumberOfConfigs = size(myFigure.StoredConfigurations,2);
 NumberOfSamples = 100*(NumberOfConfigs-1);
 [q,qd,qdd,tSamp] = trapveltraj(myFigure.StoredConfigurations,NumberOfSamples);
 
-% task p)
+
 rateController = rateControl(NumberOfSamples/(max(tSamp) - tSamp(2))); 
 for i = 1:NumberOfSamples 
     myFigure.Configuration = q(:,i); 
@@ -82,11 +82,11 @@ plot(tSamp, minimum_distance_object);
 xlabel("t in s"); ylabel("Distance in cm"); 
 title("Distance of the gripper claws to the object")
 
-% task q)
+
 %the reason behaind the sepDist(14) is inf the TCP might not identified in
 %collosion dection setup. if the collision geometry for TCP or endeffector
 %is not properly configured then the algoridham cannot compute the distance
 %correctly.
 
-% task r)
+
 distance=mean(sepDist(12:13))
